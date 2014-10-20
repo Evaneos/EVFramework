7f4c7a4598a08011a3ba3140ddd75ff92191aaca
Olivier Madre <terenas@gmail.com>
2014-10-20 11:43:40 +0200

    added method getBuilders due to interface change

--- src/Berthe/RESTBuilder/BertheProvider.php
+++ src/Berthe/RESTBuilder/BertheProvider.php
@@ -18,6 +18,16 @@ class BertheProvider implements PyRestBuilderProvider
         return $this->container->get($this->getServiceName($resourceName, 'RESTBuilder'));
     }
 
+    public function getBuilders()
+    {
+        $builders = array();
+        $packages = $this->container->getParameter('crud.packages');
+        foreach($packages as $resourceName => $config) {
+            $builders[$resourceName] = $this->getBuilder($resourceName);
+        }
+        return $builders;
+    }
+
     protected function getServiceName($resourceName, $serviceName)
     {
         $definitionNameWithPlaceHolders = $this->container->getParameter('crud.configuration.definition_name');
