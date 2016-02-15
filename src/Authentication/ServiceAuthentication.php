1c8ac56209dcb0edc661180ee8c75a487b561437
Johann Saunier <saunier.johann@gmail.com>
2016-02-15 18:03:19 +0100

    use logger factory

--- src/Authentication/ServiceAuthentication.php
+++ src/Authentication/ServiceAuthentication.php
@@ -2,12 +2,9 @@
 
 namespace EVFramework\Authentication;
 
-use Evaneos\Pro\Framework\Security\EvaneosRoleManager;
-use Evaneos\Pro\Framework\Security\EmailUserDetailsService;
-use Evaneos\Pro\Framework\Security\EvaneosPasswordEncoder;
-use Trolamine\Core\Authentication\Password\PasswordEncoder;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpKernel\Log\NullLogger;
 use Trolamine\Core\Authentication\Authentication;
-use Trolamine\Core\Authentication\Role\RoleManager;
 use Trolamine\Core\Authentication\AuthenticationManager;
 use Trolamine\Core\Authentication\UsernamePasswordAuthenticationToken;
 use Trolamine\Core\Exception\BadCredentialsException;
@@ -15,17 +12,47 @@ use Trolamine\Core\Exception\InsufficientAuthenticationException;
 
 class ServiceAuthentication
 {
+    /**
+     * @var AuthenticationManager
+     */
     private $authenticationManager;
 
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /** @var Authentication */
+    private $auth;
+
+    /**
+     * ServiceAuthentication constructor.
+     *
+     * @param AuthenticationManager $authenticationManager
+     */
+    public function __construct(AuthenticationManager $authenticationManager)
+    {
+        $this->authenticationManager = $authenticationManager;
+    }
+
+    /**
+     * @param AuthenticationManager $authenticationManager
+     *
+     * @return $this
+     */
     public function setAuthenticationManager(AuthenticationManager $authenticationManager)
     {
+        $this->logger = new NullLogger();
         $this->authenticationManager = $authenticationManager;
         return $this;
     }
 
-    public function __construct(AuthenticationManager $authenticationManager)
+    /**
+     * @param LoggerInterface $logger
+     */
+    public function setLogger(LoggerInterface $logger)
     {
-        $this->authenticationManager = $authenticationManager;
+        $this->logger = $logger;
     }
 
     /**
@@ -37,12 +64,21 @@ class ServiceAuthentication
      */
     public function login($email, $password)
     {
+        if(true === $this->isLoggedIn()){ //already logged
+            return $this->auth;
+        }
+
         session_regenerate_id(true);
+
         try {
-            return $this->authenticationManager->authenticate(new UsernamePasswordAuthenticationToken($email, $password));
+            $this->auth = $this->authenticationManager->authenticate(new UsernamePasswordAuthenticationToken($email, $password));
+            $this->logger->notice('User connected', array('email' => $email));
+            return $this->auth;
         } catch (InsufficientAuthenticationException $e) {
+            $this->logger->notice('Insufficient permission', array('email' => $email));
             return null;
         } catch (BadCredentialsException $e) {
+            $this->logger->notice('Bad credential', array('email' => $email));
             return null;
         }
     }
@@ -52,6 +88,16 @@ class ServiceAuthentication
      */
     public function isLoggedIn(Authentication $authentication = null)
     {
-        return ($authentication && $authentication->getAuthenticationMode() == Authentication::FULLY_AUTHENTICATED);
+        if(null === $this->auth && null === $authentication){
+            return false;
+        }
+
+        if(null === $authentication){
+            $authentication = $this->auth;
+        }
+
+        dump($authentication->getAuthenticationMode());
+
+        return $authentication->getAuthenticationMode() == Authentication::FULLY_AUTHENTICATED;
     }
 }
