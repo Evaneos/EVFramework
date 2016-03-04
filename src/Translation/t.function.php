325266b0255d017937303f8dcbcd6755aeb28040
Johann Saunier <ProPheT777@users.noreply.github.com>
2016-03-04 13:08:55 +0100

    Update t.function.php

--- src/Translation/t.function.php
+++ src/Translation/t.function.php
@@ -1,5 +1,23 @@
 <?php
 function t($key, $lang = null)
 {
-    return \Translation_Manager::getInstance()->translate($key, $lang);
+    if (!$lang) {
+        $lang = 'fr';
+    }
+
+
+    if ($key instanceof \Berthe\Translation\Translation) {
+        $translation = $key->format($lang);
+    } else {
+        $translation = \Translation_Manager::getInstance()->translate($key, $lang);
+    }
+
+    // If more than 2 args, we
+    if (func_num_args() > 2) {
+        $args = array_slice(func_get_args(), 2);
+        array_unshift($args, $translation);
+        return call_user_func_array('sprintf', $args);
+    }
+
+    return $translation;
 }
